0dfab21fff89166b5e9c528038408ce208636c2b
Daniel Schmitz <danieljfa@gmail.com>
2023-08-15 11:53:12 -0300

    ✨ Create user (backend, swagger)

--- backend/api/users.ts
+++ backend/api/users.ts
@@ -1,10 +1,10 @@
 import { Prisma, PrismaClient } from '@prisma/client'
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const prisma = new PrismaClient()
 const app = express()
 
-app.get('/users', async (req, res) => {
+app.get('/users', async (req: Request, res: Response) => {
   /*
     #swagger.tags = ['Users']
     #swagger.summary = 'Get all users'
@@ -12,6 +12,41 @@ app.get('/users', async (req, res) => {
     */
   const users = await prisma.user.findMany()
   res.json(users)
+})
+
+app.post('/user', async function (req: Request, res: Response) {
+  /*
+    #swagger.tags = ['Users']
+    #swagger.summary = 'Add a user'
+    #swagger.responses[403] = { description: 'Invalid Input' }
+    #swagger.responses[201] = { description: "User created" }
+    #swagger.responses[409] = { description: 'User already exists' }
+    #swagger.parameters['user'] = {
+        in: 'body',
+        description: 'User Data',
+        required: true,
+        schema: { 
+            "name": "User name",
+            "email": "User email"
+        }
+    } 
+    */
+  const { name, email } = req.body
+
+  // todo: validation
+
+  const userWithEmail = await prisma.user.findUnique({
+    where: { email },
+  })
+  if (userWithEmail) {
+    return res.status(409).json({ message: 'User email already exists' })
+  }
+
+  const user = await prisma.user.create({
+    data: { name, email },
+  })
+
+  return res.status(200).json(user)
 })
 
 export default app
